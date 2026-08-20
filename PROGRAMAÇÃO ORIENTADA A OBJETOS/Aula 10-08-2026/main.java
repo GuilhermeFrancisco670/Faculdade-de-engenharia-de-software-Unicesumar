e7f6e537f74e31ea2372e7af530e293484deebfd
Guilherme Francisco <gui.f.m.martinelli@gmail.com>
2026-08-20 13:29:52 -0300

    Add main class with vehicle instances and display method

---
+++ PROGRAMAÇÃO ORIENTADA A OBJETOS/Aula 10-08-2026/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void main() {
+
+
+        Veiculo uno = new Veiculo("Fiat", "Uno", 2009);
+        uno.quantPortas=4;
+        uno.exibirDados();
+
+        Veiculo fusca = new Veiculo("VW","Fusca 1300",1976);
+        fusca.exibirDados();
+    }
+}
